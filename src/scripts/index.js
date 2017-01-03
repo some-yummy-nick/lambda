0613506118b1c0d2004e5bdd9dba3aeae30f641c
yeffasol <yeffasol@mail.ru>
2017-01-03 21:03:26 +0300

    Update 2017-01-03T18:02:20.038Z

--- src/scripts/index.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var toggler = document.querySelector(".menu__toggle");
-
-  toggler.addEventListener("click", function () {
-    document.querySelector(".menu__list").classList.toggle("menu__list--hide");
-    this.classList.toggle("menu__toggle--close");
-  });
-})();
